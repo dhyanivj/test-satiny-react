1e479a3dad49dca5cf0eb50b8faa077d8c1b97f5
Vijay Dhyani <vijay97dhyani@gmail.com>
2023-01-10 10:18:31 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+import Post from './components/Post'
+import Project from './components/Project'
+import SinglePost from './components/SinglePost'
 
 const App = () => {
   return (
@@ -9,6 +12,10 @@ const App = () => {
    <Switch>
 <Route component={Home} path='/' exact/>
 <Route component={About} path='/about' />
+<Route component={SinglePost} path='/post/:slug' />
+<Route component={Post} path='/post' />
+<Route component={Project} path='/project' />
+
    </Switch>
    </BrowserRouter>
 
