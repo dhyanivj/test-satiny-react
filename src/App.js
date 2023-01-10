3665b3d490fec19975b331c1622cd13f4a8a9d0e
Vijay Dhyani <vijay97dhyani@gmail.com>
2023-01-10 10:13:08 +0000

    router and satiny cms added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 const App = () => {
   return (
-    <div>App</div>
+   <BrowserRouter>
+   <Switch>
+<Route component={Home} path='/' exact/>
+<Route component={About} path='/about' />
+   </Switch>
+   </BrowserRouter>
+
   )
 }
 
