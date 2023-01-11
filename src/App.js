361cda30ffafa61fd7fe08b7fcc19de89b014aa1
Vijay Dhyani <vijay97dhyani@gmail.com>
2023-01-11 05:35:34 +0000

    new changes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import About from './components/About'
 import Post from './components/Post'
 import Project from './components/Project'
 import SinglePost from './components/SinglePost'
+import NavBar from './components/NavBar'
+import Blog from './components/Blog'
 
 const App = () => {
   return (
    <BrowserRouter>
+   <NavBar/>
    <Switch>
 <Route component={Home} path='/' exact/>
 <Route component={About} path='/about' />
 <Route component={SinglePost} path='/post/:slug' />
 <Route component={Post} path='/post' />
 <Route component={Project} path='/project' />
-
+<Route component={Blog} path='/blog' />
    </Switch>
    </BrowserRouter>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <header className="bg-red-600">
+      <nav>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/post">
+          Post
+        </NavLink>
+        <NavLink to="/blog">
+          blog
+        </NavLink>
+        <NavLink to="/project">
+          Project
+        </NavLink>
+        <NavLink to="/about">
+          About me
+        </NavLink>
 
-export default NavBar
+
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
